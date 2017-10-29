2a8a1adc256218de72ec03284dc48cce79a703a4
Koen Luyten <Koen.luyten@student.kdg.be>
2017-10-29 17:31:18 +0100

    Section constructor & getters

---
+++ detector/src/main/java/Domain/Section.java
@@ -0,0 +1,44 @@
+package Domain;
+
+public class Section {
+
+    private  int sectionID;
+    private int numberOfBlocks;
+    private int blockLenght;
+    private String crossings;
+    private boolean singleDirection;
+    private int[] highBlocknumberSectionIds;
+
+    public Section(int sectionID, int numberOfBlocks, int blockLenght, String crossings, boolean singleDirection, int[] highBlocknumberSectionIds){
+        this.sectionID = sectionID;
+        this.numberOfBlocks = numberOfBlocks;
+        this.blockLenght = blockLenght;
+        this.crossings = crossings;
+        this.singleDirection = singleDirection;
+        this.highBlocknumberSectionIds = highBlocknumberSectionIds;
+    }
+
+    public int getSectionID() {
+        return sectionID;
+    }
+
+    public int getNumberOfBlocks() {
+        return numberOfBlocks;
+    }
+
+    public int getBlockLenght() {
+        return blockLenght;
+    }
+
+    public String getCrossings() {
+        return crossings;
+    }
+
+    public boolean isSingleDirection() {
+        return singleDirection;
+    }
+
+    public int[] getHighBlocknumberSectionIds() {
+        return highBlocknumberSectionIds;
+    }
+}
