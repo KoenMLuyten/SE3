33723d596b27df4c058fc9a282dbe08533bdda30
Koen Luyten <Koen.luyten@student.kdg.be>
2017-11-08 13:08:59 +0100

    tests included

--- detector/src/test/java/DummyQueue/DummyQueue.java
+++ detector/src/test/java/DummyQueue/DummyQueue.java
@@ -1,4 +1,57 @@
 package DummyQueue;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
 public class DummyQueue {
+
+    private String connectionString;
+    private String queueName;
+
+    public DummyQueue(String connectionString, String queueName){
+        this.connectionString = connectionString;
+        this.queueName = queueName;
+    }
+
+    public void start(){
+        Connection connection;
+        Channel channel;
+        try{
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setUri(connectionString);
+
+            //Recommended settings
+            factory.setRequestedHeartbeat(30);
+            factory.setConnectionTimeout(30000);
+
+            connection = factory.newConnection();
+            channel = connection.createChannel();
+
+            channel.queueDeclare(queueName, false, false, false, null);
+            String message1 = "<detectionMessage>\n" +
+                    "  <rideId>1</rideId>\n" +
+                    "  <blockNr>1</blockNr>\n" +
+                    "  <timestamp>2017-11-02 16:14:28.399</timestamp>\n" +
+                    "  <sectionId>1</sectionId>\n" +
+                    "</detectionMessage>";
+
+            String message2 = "<detectionMessage>\n" +
+                    "  <rideId>1</rideId>\n" +
+                    "  <blockNr>2</blockNr>\n" +
+                    "  <timestamp>2017-11-02 16:14:28.399</timestamp>\n" +
+                    "  <sectionId>1</sectionId>\n" +
+                    "</detectionMessage>";
+            channel.basicPublish("", queueName, null, message1.getBytes());
+            channel.basicPublish("", queueName, null, message2.getBytes());
+
+            channel.close();
+            connection.close();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+    }
 }
--- detector/src/test/java/Test.java
+++ detector/src/test/java/Test.java
@@ -1,4 +1,65 @@
-package PACKAGE_NAME;
+import Domain.*;
+import Domain.Checks.*;
+import Domain.Messages.IncomingMessageDTO;
+import Domain.Messages.OutGoingStopMessage;
+import Domain.Messages.OutgoingSpeedMessage;
+import DummyQueue.DummyQueue;
+import Services.Implementations.*;
+import Services.Interfaces.*;
+import com.thoughtworks.xstream.XStream;
+
+import java.util.ArrayList;
+
 
 public class Test {
+    public static void main(String[] args) {
+        IRouteService routeService = new RouteService("www.services4se3.com/railway/route/");
+        IInfrastructureService infrastructureService = new InfraService("www.services4se3.com/railway/infrastructure/sections/");
+        XStream xStream = new XStream();
+
+        InfrastructureHandler infrastructureHandler = new InfrastructureHandler(infrastructureService);
+        RouteHandler routeHandler = new RouteHandler(routeService);
+
+        xStream.aliasField("rideId", IncomingMessageDTO.class, "lastAttribute");
+        xStream.aliasField("closed",IncomingMessageDTO.class, "lastAttribute");
+        xStream.alias("StopMessage", OutGoingStopMessage.class);
+        xStream.alias("SpeedMessage", OutgoingSpeedMessage.class);
+        xStream.alias("detectionMessage", IncomingMessageDTO.class);
+        xStream.alias("signalisationMessage", IncomingMessageDTO.class);
+
+        MessageFormatter formatter = new XStreamFormatter(xStream);
+
+        final String inMessageString = "amqp://hjnwvnem:IZWi2g4eMtJlxOVGMNbMkvzYvuu7patt@elephant.rmq.cloudamqp.com/hjnwvnem";
+        final String detectionQueueName = "detection_queue";
+        final String signalisationQueueName = "signal_queue";
+        final String outMessageString = "amqp://zhghektw:gOYMs97z_HaHm9ARlsNKQvePouKZfycj@elephant.rmq.cloudamqp.com/zhghektw";
+
+        IncomingMessageService inMessageService = new IncomingRabbitMq(inMessageString, detectionQueueName, formatter);
+        OutgoingMessageService outMessageService = new OutgoingRabbitMq(outMessageString, formatter);
+
+        MessageRecorder inRecorder = new MessageRecorder("input.txt");
+        MessageRecorder outRecorder = new MessageRecorder("output.txt");
+
+        inMessageService.setRecorder(inRecorder);
+        outMessageService.setRecorder(outRecorder);
+
+        ArrayList<IChecker> checkers = new ArrayList<>();
+        checkers.add(new H2HChecker(infrastructureHandler, routeHandler));
+        checkers.add(new H2TChecker(routeHandler, infrastructureHandler, 5));
+        checkers.add(new SignChecker(infrastructureHandler, routeHandler, 5));
+        IChecker checker = new CompositeChecker(checkers);
+
+
+        DetectionHandler detectionHandler = new DetectionHandler(checker, routeHandler);
+        SignalisationHandler signalisationHandler = new SignalisationHandler();
+
+        MessageListener detectionListener = new DetectionListener(detectionHandler, inMessageService, outMessageService, detectionQueueName, 20);
+        MessageListener signalisationListener = new SignalisationListener(signalisationHandler, inMessageService, signalisationQueueName, 20);
+
+
+        DummyQueue dummyQueue = new DummyQueue(inMessageString,detectionQueueName);
+        dummyQueue.start();
+        signalisationListener.start();
+        detectionListener.start();
+    }
 }
