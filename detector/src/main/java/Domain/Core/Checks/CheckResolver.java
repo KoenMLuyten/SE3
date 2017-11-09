cd46313c210b4f5c75edeb7ca334ab680a2cbcd0
Koen Luyten <Koen.luyten@student.kdg.be>
2017-11-09 10:41:25 +0100

    laatste commit

--- detector/src/main/java/Domain/Core/Checks/CheckResolver.java
+++ detector/src/main/java/Domain/Core/Checks/CheckResolver.java
@@ -5,6 +5,8 @@ import Domain.Entitities.Actions.SpeedAction;
 import Domain.Entitities.Actions.StopAction;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+
 /*
 * Class responsible for resolving all the checkresults CompositeChecker receives from it's different checks
 * */
@@ -15,39 +17,46 @@ public class CheckResolver {
     * @Param toResolve Arraylist<CheckResult>
     * @return CheckResult
     * */
-    public static CheckResult resolve(ArrayList<CheckResult> toResolve){
+    public static synchronized CheckResult resolve(ArrayList<CheckResult> toResolve){
         CheckResult finalOut;
         ArrayList<CheckAction> actionsOut = new ArrayList<>();
         for (CheckResult result : toResolve) {
             if (result.isActionRequired()){
-                result.getRequiredActions().forEach(checkAction -> {
+                Iterator<CheckAction> checkActionIterator= result.getRequiredActions().iterator();
+                while (checkActionIterator.hasNext()) {
+                    CheckAction checkAction = checkActionIterator.next();
                     if (checkAction.getClass() == StopAction.class){                                                    //Checks whether a certain StopAction needs to be included in the output
                         if(!(actionsOut.contains(checkAction))){                                                        //If there's no stopAction there it should be included
                             actionsOut.add(checkAction);
                         }
                         if(!(actionsOut.contains(new SpeedAction(checkAction.getRideId(), 0, 0)))){     //If there's SpeedActions already in the output with the same rideId, remove these
-                            actionsOut.forEach(slowAction -> {
+                            Iterator<CheckAction> outIterator = actionsOut.iterator();
+                            while (outIterator.hasNext()) {
+                                CheckAction slowAction = outIterator.next();
                                 if(slowAction instanceof SpeedAction && checkAction.getRideId() == slowAction.getRideId()){
                                     actionsOut.remove(slowAction);
                                 }
-                            });
+                            }
                         }
                     }
                     if (checkAction instanceof SpeedAction ){                                                           //Checks whether a certain SpeedActions needs to be included in the output
                         if(!actionsOut.contains(new StopAction(checkAction.getRideId()))){                              //If there's already a stopAction with this rideId included in the output, don't include this SpeedAction
                             if(actionsOut.contains(new SpeedAction(checkAction.getRideId(), 0, 0))){
-                                actionsOut.forEach(slowAction -> {
-                                    if (slowAction instanceof SpeedAction && slowAction.equals(checkAction) && ((SpeedAction)slowAction).getSpeed() < ((SpeedAction) checkAction).getSpeed()){
-                                        actionsOut.remove(slowAction);                                                  // if there are SpeedActions concerning the same rideId as this, but with higher speed, remove them.
+                                Iterator<CheckAction> outIterator = actionsOut.iterator();
+                                while (outIterator.hasNext()) {
+                                    CheckAction slowAction = outIterator.next(); {
+                                        if (slowAction instanceof SpeedAction && slowAction.equals(checkAction) && ((SpeedAction)slowAction).getSpeed() < ((SpeedAction) checkAction).getSpeed()){
+                                            actionsOut.remove(slowAction);                                                  // if there are SpeedActions concerning the same rideId as this, but with higher speed, remove them.
+                                        }
                                     }
-                                });
-                            }
-                            if(!actionsOut.contains(new SpeedAction(checkAction.getRideId(), 0, 0))){
-                                actionsOut.add(checkAction);                                                            // if there are no other speedActions left in the output (ergo, this one was the slowest) include it in the output
+                                }
+                                if(!actionsOut.contains(new SpeedAction(checkAction.getRideId(), 0, 0))){
+                                    actionsOut.add(checkAction);                                                            // if there are no other speedActions left in the output (ergo, this one was the slowest) include it in the output
+                                }
                             }
                         }
                     }
-                });
+                }
             }
         }
 
